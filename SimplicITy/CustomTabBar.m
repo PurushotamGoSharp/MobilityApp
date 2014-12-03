72c423ca4d2e7303aa07d740ef7ec362b41c4d6f
VMoksha <vmmobility@gmail.com>
2014-12-03 15:30:42 +0530

    Committing before tab implementation

---
+++ SimplicITy/CustomTabBar.m
@@ -0,0 +1,31 @@
+//
+//  CustomTabBar.m
+//  SimplicITy
+//
+//  Created by Varghese Simon on 12/3/14.
+//  Copyright (c) 2014 Vmoksha. All rights reserved.
+//
+
+#import "CustomTabBar.h"
+
+@implementation CustomTabBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
