37f6bd0ac1043dff8885f6f03c890b0a25c7be90
VMoksha <vmmobility@gmail.com>
2014-12-11 18:12:29 +0530

    c

---
+++ SimplicITy/TicketsListCell.m
@@ -0,0 +1,32 @@
+//
+//  TicketsListCell.m
+//  SimplicITy
+//
+//  Created by Varghese Simon on 12/11/14.
+//  Copyright (c) 2014 Vmoksha. All rights reserved.
+//
+
+#import "TicketsListCell.h"
+
+@interface TicketsListCell ()
+@property (weak, nonatomic) IBOutlet UIView *colorCodeView;
+@property (weak, nonatomic) IBOutlet UILabel *ticketHeadingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *agentAssignedLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *noOfDaysBeforeRaised;
+
+@end
+
+@implementation TicketsListCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ SimplicITy/TicketsListViewController.m
@@ -0,0 +1,49 @@
+//
+//  TicketsListViewController.m
+//  SimplicITy
+//
+//  Created by Varghese Simon on 12/11/14.
+//  Copyright (c) 2014 Vmoksha. All rights reserved.
+//
+
+#import "TicketsListViewController.h"
+
+@interface TicketsListViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+@implementation TicketsListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 4;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
