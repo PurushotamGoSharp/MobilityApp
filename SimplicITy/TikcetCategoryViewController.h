6fd3f1d98d4c836866fc4b1fa5872b532fc29558
VMoksha <vmmobility@gmail.com>
2014-12-15 12:14:12 +0530

    starting to add category vc for raise a ticket

---
+++ SimplicITy/TikcetCategoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  TikcetCategoryViewController.h
+//  SimplicITy
+//
+//  Created by Varghese Simon on 12/15/14.
+//  Copyright (c) 2014 Vmoksha. All rights reserved.
+//
+
+#import "CustomColoredViewController.h"
+
+@interface TikcetCategoryViewController : CustomColoredViewController
+
+@end
