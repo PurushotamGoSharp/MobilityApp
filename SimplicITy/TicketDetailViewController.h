afec411dd4faf10e4e1cc4950aae782b22d1b1a1
VMoksha <vmmobility@gmail.com>
2014-12-16 12:56:01 +0530

    c

---
+++ SimplicITy/TicketDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TicketDetailViewController.h
+//  SimplicITy
+//
+//  Created by Varghese Simon on 12/16/14.
+//  Copyright (c) 2014 Vmoksha. All rights reserved.
+//
+
+#import "CustomColoredViewController.h"
+
+@interface TicketDetailViewController : CustomColoredViewController
+
+@end
