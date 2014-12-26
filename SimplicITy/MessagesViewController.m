0578faf31d55b41edb985c948bf62bf18366285a
Manoharak <manoharak@vmokshagroup.com>
2014-12-26 20:33:55 +0530

    m

--- SimplicITy/MessagesViewController.m
+++ SimplicITy/MessagesViewController.m
@@ -42,6 +42,7 @@
     
     [back setImage:[UIImage imageNamed:@"back_Arrow"] forState:UIControlStateNormal];
     [back setTitle:@"Back" forState:UIControlStateNormal];
+    
     back.imageEdgeInsets = UIEdgeInsetsMake(0, -35, 0, 0);
     back.titleEdgeInsets = UIEdgeInsetsMake(0, -40, 0, 0);
     back.frame = CGRectMake(0, 0,80, 40);
