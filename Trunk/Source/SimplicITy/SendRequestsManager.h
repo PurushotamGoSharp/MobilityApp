7f21d7ce5fe7ebd8c93e29e9f1c406e871607c02
VMoksha <vmmobility@gmail.com>
2015-01-22 13:58:50 +0530

    C

---
+++ Trunk/Source/SimplicITy/SendRequestsManager.h
@@ -0,0 +1,15 @@
+//
+//  SendRequestsManager.h
+//  SimplicITy
+//
+//  Created by Varghese Simon on 1/22/15.
+//  Copyright (c) 2015 Vmoksha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SendRequestsManager : NSObject
+
++ (instancetype)sharedManager;
+
+@end
