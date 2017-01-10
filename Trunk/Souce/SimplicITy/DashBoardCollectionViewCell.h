c1adefba065c095b47aaf77020a642fb95f9a597
saurabh <saurabhs@vmokshagroup.com>
2017-01-10 13:35:29 +0530

    c adding cell

---
+++ Trunk/Souce/SimplicITy/DashBoardCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  DashBoardCollectionViewCell.h
+//  SimplicITy
+//
+//  Created by Saurabh on 1/10/17.
+//  Copyright © 2017 Vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashBoardCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIButton *deletButton;
+
+@end
---
+++ Trunk/Souce/SimplicITy/DashBoardCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  DashBoardCollectionViewCell.m
+//  SimplicITy
+//
+//  Created by Saurabh on 1/10/17.
+//  Copyright © 2017 Vmoksha. All rights reserved.
+//
+
+#import "DashBoardCollectionViewCell.h"
+
+@implementation DashBoardCollectionViewCell
+
+@end
